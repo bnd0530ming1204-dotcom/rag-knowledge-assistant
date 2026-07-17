6d609f5bcf144238de19827f464975e0943ddc73
Layne <1090000829@qq.com>
2026-07-17 17:23:59 +0800

    111111

--- processor/import_processor/nodes/g_node_import_milvus.py
+++ processor/import_processor/nodes/g_node_import_milvus.py
@@ -1,3 +1,7 @@
+import json
+import logging
+from typing import List, Dict, Any
+
 from processor.import_processor.base import BaseNode
 from processor.import_processor.state import ImportGraphState
 
@@ -10,7 +14,6 @@ class NodeImportMilvus(BaseNode):
     name = "node_import_milvus"
 
     def process(self, state: ImportGraphState):
-
         # 1 数据校验
         chunks_json_data, vector_dimension = self._step_1_check_inputs(state)
 
@@ -27,3 +30,39 @@ class NodeImportMilvus(BaseNode):
         state["chunks"] = update_chunks
 
         return state
+
+
+    # 步骤1 数据校验
+    def _step_1_check_inputs(self, state: Dict[str, Any]) -> tuple[List[Dict[str, Any]], int]:
+        print("node_import_milvus: 步骤1：数据校验")
+        """
+        检查
+        """
+        return None, 0
+
+    # 步骤2 结构准备
+    def _step_2_prepare_collection(self, vector_dimension: int):
+        print("node_import_milvus: 步骤2：结构准备")
+        """
+        milvus客户端+集合准备
+        """
+        # 1 客户端
+        milvus_client = None
+        return milvus_client
+
+    # 步骤3 清理旧数据
+    def _step_3_clean_old_data(self, client, chunks_json_data):
+        print("node_import_milvus: 步骤3：清理旧数据")
+        pass
+
+    # 步骤4 插入数据
+    def _step_4_insert_data(self, client, chunks_json_data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        print("node_import_milvus: 步骤4：插入数据")
+        """
+        批量插入milvus
+        """
+        return chunks_json_data
+
+if __name__ == "__main__":
+    node = NodeImportMilvus()
+    node.process(None)
