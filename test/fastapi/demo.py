9adc4a4883a2452288acab7b4cf1534fe0c7d8f3
Layne <1090000829@qq.com>
2026-07-25 11:13:08 +0800

    111111

--- test/fastapi/demo.py
+++ test/fastapi/demo.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse, FileResponse, RedirectResponse
 
 """
 pip install fastapi
@@ -8,14 +10,61 @@ uv uvicorn run demo:app
 """
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+@app.get("/new-path")
+async def new_path():
+    return {"message": "这是新接口"}
+
+@app.get("/old-path")
+async def redirect_old_path():
+    return RedirectResponse(url="/new-path", status_code=307)
+
+
+@app.get("/download/excel")
+async def download_excel():
+    excel_path = "E:/月度报表.xlsx"
+    # 返回文件并指定下载文件名
+    return FileResponse(
+        path=excel_path,
+        filename="月度报表.xlsx",
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+
+@app.get("/api/user")
+async def get_user():
+    # 等价于直接 return {"name": "张三", "age": 20}（FastAPI 自动转 JSONResponse）
+    return JSONResponse(
+        content={"name": "张三", "age": 20},
+        status_code=200,  # 可选，默认 200
+        headers={"a": "b"}  # 可选，自定义响应头
+    )
+
+
+@app.post("/items")
+def create_item(item: Item):
+    print("create_item后端接口被访问...")
+    return item
+
+
 @app.get("/read_root")
 def read_root():
     print("read_root后端接口被访问...")
-    print("read_root后端接口被访问...")
-    print("read_root后端接口被访问...")
-    return {"hello":"world"}
+    return {"hello": "world"}
+
+
+@app.get("/item/{item_id}")
+def read_item(item_id: int, a: str):
+    print("read_item后端有参数接口被访问...")
+    return {"item_id": item_id, "a": a}
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="192.168.63.38", port=8001)
+
+    uvicorn.run(app, host="localhost", port=8000)
---
+++ test/fastapi/demo0.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from fastapi import FastAPI
+from starlette.responses import StreamingResponse
+
+app = FastAPI()
+
+
+# 普通的流式方法，用了yield没用return
+async def generate_stream():
+    words = ["你", "好", "，", "这", "是", "流", "式", "响", "应"]
+    for word in words:
+        await asyncio.sleep(0.5)
+        yield word.encode("utf-8")
+
+
+# 流式输出接口
+@app.get("/stream")
+async def stream_response():
+    print("流式输出web接口")
+    return StreamingResponse(generate_stream(), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="192.168.63.26", port=8000)
